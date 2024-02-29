5ecd8ddc5a191c72f8a4e33fbea34501330cfc52
Mingee KIm <89675001+NeatyNut@users.noreply.github.com>
2024-02-29 15:14:06 +0900

    Update post_code(수정중).dart

--- fastapi/post_code(수정중).dart
+++ fastapi/post_code(수정중).dart
@@ -5,7 +5,7 @@ class Image_to_label {
   // 생성자로 받을 url
   String url;
 
-  // 클래스
+  // 클래스(원하시는대로 늘리면 됩니다.)
   List<String> text_label = [
     'cat',
     'dog',
@@ -17,7 +17,7 @@ class Image_to_label {
   ];
 
   // api주소(fetch로 json 송신 시도)
-  String api_url = '/fetch/https://127.0.0.1:5000/images=';
+  String api_url = 'https://68b2-114-202-17-6.ngrok-free.app/';
 
   // 생성자
   Image_to_label(this.url);
@@ -27,27 +27,26 @@ class Image_to_label {
 
   // json 송신 후 label return 받기
   void get_label() async {
-    String api = api_url + url;
-    print(api);
-    var real_url = Uri.https('thingproxy.freeboard.io', api);
+    // var real_url = Uri.https('thingproxy.freeboard.io', api);
+    Uri real_url = Uri.parse(api_url);
     http.Response response = await http.post(real_url,
         headers: {
           'Content-Type': 'application/json',
-          // 'ngrok-skip-browser-warning': '69420'
+          'ngrok-skip-browser-warning': '69420'
         }, // ngrok 배포 시
         body: jsonEncode(toMap()));
 
     // 리턴 받은 값 출력
     if (response.statusCode == 200) {
-      Map<String, dynamic> map = jsonDecode(response.body);
-      print(map['label']);
+      print(response.body);
     }
   }
 }
 
 // 실행
 void main() {
+  // 이미지 URL을 통해 라벨 호출하는 코드
   Image_to_label(
-          'health.chosun.com/site/data/img_dir/2023/07/17/2023071701753_0.jpg')
+          'https://health.chosun.com/site/data/img_dir/2023/07/17/2023071701753_0.jpg')
       .get_label();
 }

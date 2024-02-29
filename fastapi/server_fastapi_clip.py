de6f8c49d81b073707082ca57917c2b1d92c0a43
Mingee KIm <89675001+NeatyNut@users.noreply.github.com>
2024-02-29 15:39:46 +0900

    Update server_fastapi_clip.py

--- fastapi/server_fastapi_clip.py
+++ fastapi/server_fastapi_clip.py
@@ -1,20 +1,24 @@
 import uvicorn  # pip install uvicorn
-from fastapi import Request, FastAPI  # pip install fastapi
+from fastapi import FastAPI  # pip install fastapi
 from fastapi.middleware.cors import CORSMiddleware  # 추가된 부분 cors 문제 해결을 위한
-import json
+from pydantic import BaseModel
 
 # 모델버전
 # 1. python : 3.11.5
 # 2. uvicorn : 0.20.0
 # 3. fastapi : 0.103.0
-# 4. json5 : 0.9.6
+# 4. pydantic : 1.10.8
 
 # 수신 json형태
-# {"url" : "https://를 뺀 url", "labels" : "cat, dog, tiger, pig"}
+# {"url" : "url", "labels" : "cat, dog, tiger, pig"}
 # url + label로 받는 형태
 
 # 예측 모듈 가져오기
 import clip_model
+
+class get_json(BaseModel):
+    url : str
+    labels : str
 
 # FastAPI application
 app = FastAPI()
@@ -27,35 +31,29 @@ app.add_middleware(
     allow_origins = origins,
     allow_credentials = True,
     allow_methods = ["*"],
-    allow_headers = ["*"],
+    allow_headers = ["*"], ## 모든 헤더 허용
 )
 
-# 노출 url 설정
-@app.get("/images={url}")
-async def read_root(jsn):
-    # 노출 url 이용할 부분 url
-    url = json.loads(jsn)['url']
+@app.post("/")
+async def read_root(jsn:get_json):
     
-    # 분류 클래스 항목
-    labels = json.loads(jsn)['labels'].split(",")
+    # labels 받기
+    labels = jsn.labels.split(",")
     
-    # 이미지 url
-    image_url = "https://" + url
-
-    # clip모델 실행
+    # url 받기
+    image_url = jsn.url
+    
+    # 모델 예측하기
     result = await clip_model.predict_text_from_image(image_url, labels)
     
-    # json 리턴
-    return {'label' : result}
+    # string 리턴
+    return result
 
 # Run the server
 if __name__ == "__main__":
     uvicorn.run("server_fastapi_clip:app",
                 reload = True,
-
-                # 호스트 주소(127.0.0.1:5000)
                 host= "127.0.0.1",
                 port=5000,
-                
                 log_level="info"
                 )

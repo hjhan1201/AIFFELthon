53ab446eb5b298b3dff61af2ddc0cd080f802df3
Mingee KIm <89675001+NeatyNut@users.noreply.github.com>
2024-03-14 12:37:18 +0900

    Update pyhon_post.py

--- fastapi/pyhon_post.py
+++ fastapi/pyhon_post.py
@@ -1,8 +1,8 @@
 import requests
 
 # URL 및 JSON 데이터 정의
-url = "https://e27f-114-202-17-6.ngrok-free.app/" # 직접 수정 필요
-data = {"url": "https://health.chosun.com/site/data/img_dir/2023/07/17/2023071701753_0.jpg", "labels": "cat, dog, rabbit, spoon, computer, tv, mouse"}
+url = "https://8a3a-114-202-17-6.ngrok-free.app"
+data = {"path": "my.jpg", "labels": "cat, dog, rabbit, spoon, computer, tv, mouse"}
 
 # headers 정의 (필요에 따라)
 headers = {"Content-Type": "application/json"}
@@ -15,3 +15,5 @@ if response.status_code == 200:
     # 성공
     response_data = response.json()
     print(response_data)
+else :
+    print("실패")
